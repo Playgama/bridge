4bdb27b7e96b85af01941a62e4d706fc5cebf457
Davit <davitsedrakian@gmail.com>
2025-07-21 18:11:18 +0400

    added prerolls

--- src/platform-bridges/BitquestPlatformBridge.js
+++ src/platform-bridges/BitquestPlatformBridge.js
@@ -68,6 +68,8 @@ class BitquestPlatformBridge extends PlatformBridgeBase {
                             this._isInitialized = true
                             this.#setupAdvertisementHandlers()
 
+                            this.showPreRoll()
+
                             this._resolvePromiseDecorator(ACTION_NAME.INITIALIZE)
                         })
                         .catch((e) => {
@@ -190,6 +192,10 @@ class BitquestPlatformBridge extends PlatformBridgeBase {
 
     showInterstitial() {
         this._platformSdk.advertisement.showInterstitial()
+    }
+
+    showPreRoll() {
+        this._platformSdk.advertisement.showPreRoll()
     }
 
     showBanner() {
