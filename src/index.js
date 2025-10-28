4b691284c298208d784e7b4042f40b2801c5cbc0
Davit <davitsedrakian@gmail.com>
2025-10-28 15:46:49 +0400

    second time js from cdn loading fix

--- src/index.js
+++ src/index.js
@@ -17,5 +17,7 @@
 
 import PlaygamaBridge from './PlaygamaBridge'
 
-window.bridge = new PlaygamaBridge()
-window.playgamaBridge = window.bridge
+if (!window.bridge && !window.playgamaBridge) {
+    window.bridge = new PlaygamaBridge()
+    window.playgamaBridge = window.bridge
+}
