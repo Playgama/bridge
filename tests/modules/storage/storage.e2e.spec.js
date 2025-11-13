1733240ba1d84b699de78a60d6162ea197286bfe
Whooooop <Whooooop>
2025-11-13 19:08:46 +0400

    Add additional storage tests for QA Tool and Playgama platforms, including multiple key retrieval and cloud storage availability checks for unauthorized players.

--- tests/modules/storage/storage.e2e.spec.js
+++ tests/modules/storage/storage.e2e.spec.js
@@ -15,8 +15,7 @@ describe('StorageModule (integration, PlaygamaBridge)', () => {
 
 
     test.each([
-        PLATFORM_ID.QA_TOOL,
-        // PLATFORM_ID.PLAYGAMA
+        PLATFORM_ID.QA_TOOL
     ])(`Storage.get single key with ${STORAGE_TYPE.LOCAL_STORAGE} for %s platform`, async (platformId) => {
         const storageType = STORAGE_TYPE.LOCAL_STORAGE
 
@@ -29,5 +28,32 @@ describe('StorageModule (integration, PlaygamaBridge)', () => {
         const value = await bridge.storage.get('test', storageType)
 
         expect(value).toBe('value_1')
+    })
+
+    test.each([
+        PLATFORM_ID.QA_TOOL
+    ])(`Storage.get multiple keys with ${STORAGE_TYPE.LOCAL_STORAGE} for %s platform`, async (platformId) => {
+        const storageType = STORAGE_TYPE.LOCAL_STORAGE
+
+        const { bridge, mockPlatformAction } = await createBridgeByPlatformId(platformId, { supportedFeatures: [SUPPORTED_FEATURES.STORAGE_LOCAL] })
+        expect(bridge.storage.isSupported(storageType)).toBe(true)
+        expect(bridge.storage.isAvailable(storageType)).toBe(true)
+
+        mockPlatformAction('storage.get', ([key1, key2]) => ({ [key1]: 'value_1', [key2]: 'value_2' }))
+
+        const value = await bridge.storage.get(['test', 'test_2'], storageType)
+
+        expect(value).toEqual(['value_1', 'value_2'])
+    })
+
+    test.each([
+        PLATFORM_ID.QA_TOOL,
+        PLATFORM_ID.PLAYGAMA
+    ])(`Cloud Storage should be not available for unauthorized player on %s platform`, async (platformId) => {
+        const storageType = STORAGE_TYPE.PLATFORM_INTERNAL
+
+        const { bridge } = await createBridgeByPlatformId(platformId, { supportedFeatures: [SUPPORTED_FEATURES.STORAGE_INTERNAL] })
+        expect(bridge.storage.isSupported(storageType)).toBe(true)
+        expect(bridge.storage.isAvailable(storageType)).toBe(false)
     })
 })
