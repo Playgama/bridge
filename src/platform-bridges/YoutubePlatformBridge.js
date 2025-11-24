0136e79639e4f3050d28b3c2cbb9b5d99fa2fd4c
Davit <davitsedrakian@gmail.com>
2025-11-25 00:04:05 +0400

    removed youtube default integration

--- src/platform-bridges/YoutubePlatformBridge.js
+++ src/platform-bridges/YoutubePlatformBridge.js
@@ -16,10 +16,11 @@
  */
 
 import PlatformBridgeBase from './PlatformBridgeBase'
-import { addJavaScript, waitFor } from '../common/utils'
+import { waitFor } from '../common/utils'
 import {
     PLATFORM_ID,
     ACTION_NAME,
+    EVENT_NAME,
     STORAGE_TYPE,
     PLATFORM_MESSAGE,
     LEADERBOARD_TYPE,
@@ -27,7 +28,7 @@ import {
     INTERSTITIAL_STATE,
 } from '../constants'
 
-const SDK_URL = 'https://www.youtube.com/game_api/v1'
+// const SDK_URL = 'https://www.youtube.com/game_api/v1'
 
 class YoutubePlatformBridge extends PlatformBridgeBase {
     // platform
@@ -72,44 +73,40 @@ class YoutubePlatformBridge extends PlatformBridgeBase {
         if (!promiseDecorator) {
             promiseDecorator = this._createPromiseDecorator(ACTION_NAME.INITIALIZE)
 
-            addJavaScript(SDK_URL).then(() => {
-                waitFor('ytgame').then(() => {
-                    this._platformSdk = window.ytgame
-                    this._defaultStorageType = STORAGE_TYPE.PLATFORM_INTERNAL
-                    this._audioStateAggregator.setState('platform', !this._platformSdk.system.isAudioEnabled())
-
-                    const getLanguagePromise = this._platformSdk.system.getLanguage()
-                        .then((language) => {
-                            this.#platformLanguage = language.length > 2 ? language.slice(0, 2) : language
-                        })
-
-                    this._platformStorageCachedData = {}
-                    const getDataPromise = this._platformSdk.game.loadData()
-                        .then((data) => {
-                            if (typeof data === 'string' && data !== '') {
-                                this._platformStorageCachedData = JSON.parse(data)
-                            }
-                        })
-
-                    this._platformSdk.system.onAudioEnabledChange((isEnabled) => {
-                        this._setAudioState(isEnabled)
+            waitFor('ytgame').then(() => {
+                this._platformSdk = window.ytgame
+                this._defaultStorageType = STORAGE_TYPE.PLATFORM_INTERNAL
+                const getLanguagePromise = this._platformSdk.system.getLanguage()
+                    .then((language) => {
+                        this.#platformLanguage = language.length > 2 ? language.slice(0, 2) : language
                     })
 
-                    this._platformSdk.system.onPause(() => {
-                        this._setPauseState(true)
+                this._platformStorageCachedData = {}
+                const getDataPromise = this._platformSdk.game.loadData()
+                    .then((data) => {
+                        if (typeof data === 'string' && data !== '') {
+                            this._platformStorageCachedData = JSON.parse(data)
+                        }
                     })
 
-                    this._platformSdk.system.onResume(() => {
-                        this._setPauseState(false)
-                    })
-
-                    Promise.all([getLanguagePromise, getDataPromise])
-                        .finally(() => {
-                            this._isInitialized = true
-                            this._resolvePromiseDecorator(ACTION_NAME.INITIALIZE)
-                            this._platformSdk.game.firstFrameReady()
-                        })
+                this._platformSdk.system.onAudioEnabledChange((isEnabled) => {
+                    this._setAudioState(isEnabled)
                 })
+
+                this._platformSdk.system.onPause(() => {
+                    this._setPauseState(true)
+                })
+
+                this._platformSdk.system.onResume(() => {
+                    this._setPauseState(false)
+                })
+
+                Promise.all([getLanguagePromise, getDataPromise])
+                    .finally(() => {
+                        this._isInitialized = true
+                        this._resolvePromiseDecorator(ACTION_NAME.INITIALIZE)
+                        this._platformSdk.game.firstFrameReady()
+                    })
             })
         }
 
@@ -250,6 +247,14 @@ class YoutubePlatformBridge extends PlatformBridgeBase {
     sendMessage(message) {
         switch (message) {
             case PLATFORM_MESSAGE.GAME_READY: {
+                const currentAudioState = this._platformSdk.system.isAudioEnabled()
+                setTimeout(() => {
+                    this.emit(EVENT_NAME.AUDIO_STATE_CHANGED, !currentAudioState)
+                    setTimeout(() => {
+                        this.emit(EVENT_NAME.AUDIO_STATE_CHANGED, currentAudioState)
+                    }, 100)
+                }, 200)
+
                 this._platformSdk.game.gameReady()
                 return Promise.resolve()
             }
