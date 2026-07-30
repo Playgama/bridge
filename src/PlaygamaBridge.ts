b72a4b438fafe36bd46697361963fb2c38a79ee7
Sergei <sergei@playgama.com>
2026-07-30 12:17:16 +0300

    Fix logo

--- src/PlaygamaBridge.ts
+++ src/PlaygamaBridge.ts
@@ -317,7 +317,7 @@ class PlaygamaBridge {
             const showFullLogo = bridge.platformId === PLATFORM_ID.YANDEX
                 || bridge.platformId === PLATFORM_ID.Y8
                 ? false
-                : options.showFullLoadingLogo === true
+                : options.showFullLoadingLogo !== false
             const showLoadingText = bridge.platformId === PLATFORM_ID.XIAOMI
                 || options.showLoadingText === true
             this.#loadingScreen = new LoadingScreen()
